033bb8d271cdeca893ec9b02a25c5e8f2712880f
Aditya123666 <Adityanugrahaadha9@gmail.com>
2018-01-31 17:31:30 +0700

    comro

--- app/src/main/java/camp/bso/inf/myapplication/RecycleView.java
+++ app/src/main/java/camp/bso/inf/myapplication/RecycleView.java
@@ -9,5 +9,6 @@ public class RecycleView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle_view);
+        //contoh RV https://medium.com/@dimas_vm/tutorial-android-recyclerview-dan-cardview-9a62aaa6cc0c
     }
 }
